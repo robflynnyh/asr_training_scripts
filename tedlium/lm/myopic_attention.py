stupid masking bug

tedlium/lm/myopic_attention.py
@@ -6,9 +6,9 @@ from torch.utils.checkpoint import checkpoint # # gradient/activation checkpoint
 
 from torch_scatter import scatter # can remove this dependency if you want (used by learnable_myopic_OLD)
 
-class GumbelSigmoid():
+class GumbelSigmoid(nn.Module):
     """
-    TAKEN FROM: https://github.com/yandexdataschool/gumbel_lstm/blob/master/gumbel_sigmoid.py
+    adapted : https://github.com/yandexdataschool/gumbel_lstm/blob/master/gumbel_sigmoid.py
 
     A gumbel-sigmoid nonlinearity with gumbel(0,1) noize
     In short, it's a function that mimics #[a>0] indicator where a is the logit
@@ -35,21 +35,40 @@ class GumbelSigmoid():
     :returns: a callable that can (and should) be used as a nonlinearity
     
     """
-    def __init__(self, t=0.1, eps=1e-20):
-        self.temperature=t
+    def __init__(
+            self, 
+            init_temperature=1.0,
+            final_temperature=0.1,
+            total_steps=10000, 
+            eps=1e-20,
+            
+        ):
+        super().__init__()
+        self.temperature=init_temperature
+        self.final_temperature=final_temperature
+        self.total_steps=total_steps
+        self.decay = (final_temperature/init_temperature)**(1/total_steps)
         self.eps=eps
+
+    def step(self):
+        if self.temperature > self.final_temperature:
+            self.temperature *= self.decay
          
     def __call__(self,logits):
         """computes a gumbel sigmoid sample"""
-                
+        temperature = self.temperature if self.training else self.final_temperature
+        
         #sample from Gumbel(0, 1)
-        uniform1 = torch.rand(logits.shape, device=logits.device, dtype=logits.dtype)
-        uniform2 = torch.rand(logits.shape, device=logits.device, dtype=logits.dtype)
+        uniform1 = torch.rand_like(logits)
+        uniform2 = torch.rand_like(logits)
         
         noise = -torch.log(torch.log(uniform2 + self.eps)/torch.log(uniform1 + self.eps) +self.eps)
-        
         #draw a sample from the Gumbel-Sigmoid distribution
-        return ((logits + noise) / self.temperature).sigmoid()
+        gumbel = ((logits + noise) / temperature).sigmoid()
+        if self.training:
+            self.step()
+        return gumbel
+        
 
 
 
@@ -99,6 +118,230 @@ class DynamicPositionBias(nn.Module):
         bias = pos[indices]
         bias = rearrange(bias, 'i j h -> h i j')
         return bias
+
+class ReLUSquared(nn.Module):
+    def forward(self, x):
+        return torch.pow(F.relu(x), 2)
+
+
+class SequenceMaskingAttention(nn.Module):
+    def __init__(
+        self,
+        n_feats,
+        head_dim,
+        n_heads,
+        dropout=0.0,
+        sequence_dropout=0.1,
+        bias=False,
+        return_attention=False,
+        causal=False,
+        activation='softmax',
+        **kwargs
+    ):
+        super().__init__()
+        assert activation in ['relusq', 'softmax']
+        self.n_feats = n_feats
+        self.head_dim = head_dim
+        self.n_heads = n_heads
+        self.dropout = nn.Dropout(dropout)
+        self.bias = bias
+        self.return_attention = return_attention
+
+        self.causal = causal
+
+        self.scale = head_dim ** -0.5
+
+        self.sequence_dropout = sequence_dropout
+
+        self.activation = ReLUSquared() if activation == 'relusq' else nn.Softmax(dim=-1)
+
+
+        self.qkv_proj = nn.Linear(n_feats, 3 * n_heads * head_dim, bias=bias)
+        self.out_proj = nn.Linear(n_heads * head_dim, n_feats, bias=bias)
+
+    def standard_forward(self, qkv, mask, pos_fn): 
+        query, key, value = qkv
+        dots = torch.einsum('bhid,bhjd->bhij', query, key) * self.scale
+        dots += pos_fn(dots.shape[-1], device=dots.device, dtype=dots.dtype)
+        qkmask = ~mask
+        attn_mask = ~(rearrange(qkmask, "b n -> b () n ()") * rearrange(qkmask, "b n -> b () () n"))
+    
+        if self.causal: # create a regular causal mask
+            causal_mask = torch.ones(dots.shape[-2], dots.shape[-1], device=dots.device).triu(1).bool()
+            attn_mask = torch.logical_or(attn_mask, causal_mask)
+        
+        dots.masked_fill_(attn_mask, -torch.finfo(dots.dtype).max)
+    
+        attn = self.activation(dots)   
+        attn = self.dropout(attn)
+        return torch.einsum("bhij,bhjd->bhid", attn, value)
+        
+
+    def forward(self, x, pos_fn, mask=None, return_attention=False, standard_attention=False):
+        assert pos_fn is not None, 'pls provide a position function'
+        B, N, C, H, D = *x.shape, self.n_heads, self.head_dim
+        #print(x.shape, mask.shape)
+
+        standard_attention = standard_attention if self.training else True # we don't want to use dropout during inference
+
+        if mask is None:
+            mask = torch.zeros(B, N, device=x.device, dtype=torch.bool)
+
+        qkv = rearrange(self.qkv_proj(x), "b n (h d qkv) -> qkv b h n d", qkv=3, h=H, d=D) # qkv projection
+        if not standard_attention:
+            q, k, v = qkv
+            # get a random sequence of zero or ones that is a quarter of the sequence length
+            seq_mask = torch.randint(0, 100, (B, H, max(N // 4,1)), device=x.device, dtype=torch.int8) < (self.sequence_dropout * 100)
+            seq_mask = seq_mask.repeat_interleave(5, dim=-1)[..., :N] # repeat the mask to be the same length as the sequence
+            
+            if seq_mask.shape[-1] != N:
+                diff = N - seq_mask.shape[-1]
+                pad_seq = torch.randint(0, 100, (B, H, diff), device=x.device, dtype=torch.int8) < (self.sequence_dropout * 100)
+                seq_mask = torch.cat((seq_mask, pad_seq), dim=-1)
+
+
+            dots = einsum("b h i d, b h j d -> b h i j", q, k) * self.scale # dot product between the queries and keys
+            
+            # positional stuff
+            pos_bias = pos_fn(N, device=dots.device, dtype=dots.dtype)
+            dots = dots + pos_bias
+
+    
+            qkmask = ~mask
+            attn_mask = ~(rearrange(qkmask, "b n -> b () n ()") * rearrange(qkmask, "b n -> b () () n"))
+            
+            if self.causal:
+                causal_mask = torch.ones(dots.shape[-2], dots.shape[-1], device=dots.device).triu(1).bool()
+                attn_mask = torch.logical_or(attn_mask, causal_mask)
+            mask_val = -torch.finfo(dots.dtype).max
+
+            dots.masked_fill_(attn_mask, mask_val)
+
+            attn = self.activation(dots)
+            
+            attn = attn.masked_fill(rearrange(seq_mask, "b h n -> b h () n"), 0) # apply the sequence mask
+
+            attn = self.dropout(attn)
+            out = einsum("bhij,bhjd->bhid", attn, v)
+        else:
+            out = self.standard_forward(qkv, mask, pos_fn)
+
+
+        out = rearrange(out, "b h n d -> b n (h d)")
+        out = self.out_proj(out)
+        return out if not return_attention else (out, attn)
+
+class SequenceDropoutAttention(nn.Module):
+    def __init__(
+        self,
+        n_feats,
+        head_dim,
+        n_heads,
+        dropout=0.0,
+        sequence_dropout=0.4,
+        bias=False,
+        return_attention=False,
+        causal=False,
+        activation='softmax',
+        just_mask=True, # if true only mask the keys don't remove them
+        **kwargs
+    ):
+        super().__init__()
+        assert activation in ['relusq', 'softmax']
+        self.n_feats = n_feats
+        self.head_dim = head_dim
+        self.n_heads = n_heads
+        self.dropout = nn.Dropout(dropout)
+        self.bias = bias
+        self.return_attention = return_attention
+
+        self.causal = causal
+
+        self.scale = head_dim ** -0.5
+
+        self.sequence_dropout = sequence_dropout
+
+        self.activation = ReLUSquared() if activation == 'relusq' else nn.Softmax(dim=-1)
+
+        self.just_mask = just_mask
+
+        self.qkv_proj = nn.Linear(n_feats, 3 * n_heads * head_dim, bias=bias)
+        self.out_proj = nn.Linear(n_heads * head_dim, n_feats, bias=bias)
+
+    def standard_forward(self, qkv, mask, pos_fn): 
+        query, key, value = qkv
+        dots = torch.einsum('bhid,bhjd->bhij', query, key) * self.scale
+        dots += pos_fn(dots.shape[-1], device=dots.device, dtype=dots.dtype)
+        attn_mask = rearrange(mask, "b n -> b () n ()") * rearrange(mask, "b n -> b () () n")
+    
+        if self.causal: # create a regular causal mask
+            causal_mask = torch.ones(dots.shape[-2], dots.shape[-1], device=dots.device).triu(1).bool()
+            attn_mask = torch.logical_or(attn_mask, causal_mask)
+        
+        dots.masked_fill_(attn_mask, -torch.finfo(dots.dtype).max)
+    
+        attn = self.activation(dots)   
+        attn = self.dropout(attn)
+        return torch.einsum("bhij,bhjd->bhid", attn, value)
+        
+
+    def forward(self, x, pos_fn, mask=None, return_attention=False, standard_attention=False):
+        assert pos_fn is not None, 'pls provide a position function'
+        B, N, C, H, D = *x.shape, self.n_heads, self.head_dim
+
+        standard_attention = standard_attention if self.training else True # we don't want to use dropout during inference
+
+        if mask is None:
+            mask = torch.zeros(B, N, device=x.device, dtype=torch.bool)
+
+        qkv = rearrange(self.qkv_proj(x), "b n (h d qkv) -> qkv b h n d", qkv=3, h=H, d=D) # qkv projection
+        if not standard_attention:
+            q, kv = qkv[0], qkv[1:] # separate q and kv, we keep kv together for now as we apply the same operations to both
+            # get a random sequence of zero or ones that is a quarter of the sequence length
+            seq_mask = torch.randint(0, 10, (B, H, max(N // 4,1)), device=x.device, dtype=torch.int8) 
+            seq_mask = seq_mask.repeat_interleave(5, dim=-1)[..., :N] # repeat the mask to be the same length as the sequence
+            if seq_mask.shape[-1] != N:
+                seq_mask = F.pad(seq_mask, (0, N - seq_mask.shape[-1]), value=4) # do this better
+            k = int(N * (1 - self.sequence_dropout)) # get the number of tokens to keep
+            # mask out seq_mask to prioritize keeping non-masked values
+            seq_mask = seq_mask.masked_fill(mask.unsqueeze(1), -1)
+            keep_indices = torch.topk(seq_mask, k, dim=-1, largest=True, sorted=False).indices.sort(dim=-1).values # get the indices to keep
+            _, kv_B, kv_H, kv_N, kv_D = kv.shape
+            # get the keys and values to keep
+            kv = kv.gather(-2, repeat(keep_indices, "b h n -> kv b h n d", kv=2, d=D)) # get the keys and values to keep
+        
+            k_mask = repeat(mask, "b n -> b h n", h=H).gather(-1, keep_indices) # get the mask for the keys
+            k, v = kv # separate the keys and values
+
+            dots = einsum("b h i d, b h j d -> b h i j", q, k) * self.scale # dot product between the queries and keys
+            
+            # positional stuff
+            pos_bias = pos_fn(N, device=dots.device, dtype=dots.dtype)
+            pos_bias = repeat(pos_bias, 'h i j -> b h i j', b = B)
+            keep_indices = repeat(keep_indices, "b h n -> b h i n", i=N)
+            pos_bias = pos_bias.gather(-1, keep_indices)
+            dots = dots + pos_bias
+        
+            qk_mask = rearrange(mask, "b n -> b () n ()") * rearrange(k_mask, "b h n -> b h () n")
+            
+            if self.causal:
+                causal_mask = keep_indices > rearrange(torch.arange(0, N, device=q.device), "n -> n ()", n=N)
+                qk_mask = torch.logical_or(qk_mask, causal_mask)
+            
+            dots.masked_fill_(qk_mask, -torch.finfo(dots.dtype).max)
+            attn = self.activation(dots)
+            attn = self.dropout(attn)
+            out = einsum("bhij,bhjd->bhid", attn, v)
+        else:
+            out = self.standard_forward(qkv, mask, pos_fn)
+
+
+        out = rearrange(out, "b h n d -> b n (h d)")
+        out = self.out_proj(out)
+        return out if not return_attention else (out, attn)
+
+
+
 
 class MyopicAttention(nn.Module):
     def __init__(
@@ -178,6 +421,7 @@ class MyopicAttention(nn.Module):
         attn = dots.softmax(dim=-1)
         
         attn = self.dropout(attn)
+        
 
         out = torch.einsum('bhij,bhjd->bhid', attn, value)
         return out
@@ -256,10 +500,12 @@ class MyopicAttention(nn.Module):
             dots.masked_fill_(qk_mask, -torch.finfo(dots.dtype).max)
 
             attn = dots.softmax(dim=-1)
+            attn = self.dropout(attn)
     
             out = einsum("b h n w z, b h n z d -> b h n w d", attn, v)
             out = rearrange(out, "b h n w d -> b (n w) (h d)")
             out = self.unpad(out, pad_n)
+           
         else:
             out = self.standard_forward(qkv=qkv, mask=mask, pos_fn=pos_fn)
             out = rearrange(out, "b h n d -> b n (h d)")
@@ -478,6 +724,7 @@ class LearnableMyopicAttentionOLD(nn.Module):
             dots.masked_fill_(qk_mask, -torch.finfo(dots.dtype).max)
 
             attn = dots.softmax(dim=-1)
+            attn = self.dropout(attn)
     
             out = einsum("b h n w z, b h n z d -> b h n w d", attn, v)
             out = rearrange(out, "b h n w d -> b (n w) (h d)")
@@ -518,12 +765,22 @@ class LearnableMyopicAttention(nn.Module):
         self.W = chunk_window
 
         self.grid_pos_projection = nn.Linear(1, head_dim*n_heads)
-        self.grid_k_projection = nn.Linear(head_dim, head_dim)
+        #self.grid_k_projection = nn.Linear(head_dim, head_dim)
+        self.grid_k_bias = nn.Parameter(torch.zeros(n_heads, 1, 1, head_dim))
+        nn.init.xavier_uniform_(self.grid_k_bias)
         self.grid_activation = nn.SiLU()
         self.grid_scaler_projection = nn.Linear(head_dim, 1)
-        self.gumbel_sigmoid = GumbelSigmoid(t=0.1, eps=1e-20)
+        
 
+        anneal_steps = 10000
+        initial_temp = 1.0
+        final_temp = 0.1
 
+        self.gumbel_sigmoid = GumbelSigmoid(
+            init_temperature = initial_temp,
+            final_temperature = final_temp,
+            total_steps = anneal_steps
+        ) 
 
         self.qkv_proj = nn.Linear(n_feats, 3 * n_heads * head_dim, bias=bias)
         self.out_proj = nn.Linear(n_heads * head_dim, n_feats, bias=bias)
@@ -550,6 +807,7 @@ class LearnableMyopicAttention(nn.Module):
         return x
 
     def valuegrid(self, total_size, block_size, k):
+        
         n = total_size // block_size
         device, dtype = k.device, k.dtype
   
@@ -561,12 +819,16 @@ class LearnableMyopicAttention(nn.Module):
         pos = pos.repeat_interleave(block_size, dim=1)
         pos = rearrange(pos, "p n (h d) -> () h p n d", h = self.n_heads, d = self.head_dim)
     
-        k_voting = self.grid_k_projection(k)
+        k_voting = k + self.grid_k_bias
         k_voting = k_voting + pos
         k_voting = self.grid_activation(k_voting)
         k_voting = self.grid_scaler_projection(k_voting).squeeze(-1)
         k_voting = k_voting / k_voting.sum(dim=-1, keepdim=True)
+      
+        #k_voting = torch.randn((k.shape[0], k.shape[1], k.shape[2], k.shape[3]), device=k.device, dtype=k.dtype)
+        #k_voting = k_voting / k_voting.sum(dim=-1, keepdim=True)
         k_voting = self.gumbel_sigmoid(k_voting)
+        #print((k_voting[0,0,0] < 0.1).sum() / k_voting.shape[-1])
         return k_voting    
    
 
@@ -602,7 +864,7 @@ class LearnableMyopicAttention(nn.Module):
 
         tokeep = min(self.max_keep_keys, N) if self.max_keep_keys != -1 else N # number of keys to keep
         W = min(self.W, N) if self.W != -1 else N # window size
-        #standard_attention = standard_attention if (W != N and tokeep != N) else True # this is equivalent to standard attention if W&tokeep == N so we can just use standard attention to save unnecessary computation
+        standard_attention = standard_attention if (W != N and tokeep != N) else True # this is equivalent to standard attention if W&tokeep == N so we can just use standard attention to save unnecessary computation
 
         qkv = rearrange(self.qkv_proj(x), "b n (h d qkv) -> qkv b h n d", qkv=3, h=H, d=D) # qkv projection
 
@@ -621,10 +883,11 @@ class LearnableMyopicAttention(nn.Module):
 
             valuegrid = self.valuegrid(total_size=N, block_size=W, k=kv[0]).to(kv.device)
             #print((valuegrid[0,0,0] < 0.1).sum() / valuegrid.shape[-1])
-            valuegrid = repeat(valuegrid, "b h w n -> kv b h w n", kv=2)
-
-            #if self.training:
-            kv = kv * valuegrid.unsqueeze(-1) # maybe just do this for the keys? idk
+            #valuegrid = repeat(valuegrid, "b h w n -> kv b h w n", kv=2)
+         
+            if self.training:
+                kv = torch.cat((kv[0].unsqueeze(0) * valuegrid.unsqueeze(-1), kv[1:]))
+            
 
             cmask = repeat(mask, 'b n -> kv b h nw n', kv=2, h=H, nw=NW)
 
@@ -666,6 +929,7 @@ class LearnableMyopicAttention(nn.Module):
             dots.masked_fill_(qk_mask, -torch.finfo(dots.dtype).max)
 
             attn = dots.softmax(dim=-1)
+            attn = self.dropout(attn)
     
             out = einsum("b h n w z, b h n z d -> b h n w d", attn, v)
             out = rearrange(out, "b h n w d -> b (n w) (h d)")
@@ -710,7 +974,7 @@ class transformer(nn.Module):
             max_keep_keys = -1, 
             W = -1,
             dropout = 0.,
-            checkpoint = True,
+            checkpoint = False,
         ):
         super().__init__()
 
@@ -726,7 +990,7 @@ class transformer(nn.Module):
         self.layers = nn.ModuleList([])
         for _ in range(depth):
             self.layers.append(nn.ModuleList([
-                PreNorm(dim, LearnableMyopicAttention(
+                PreNorm(dim, SequenceMaskingAttention(
                     dim, 
                     n_heads=heads, 
                     head_dim=dim_head, 