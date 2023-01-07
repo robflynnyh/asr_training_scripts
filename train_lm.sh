653d50b77fdca7c24f3f843e3414ba0c1d6ce3f3
Rob <rjflynn2@sheffield.ac.uk>
2023-01-07 20:22:35 +0000

    backin up

--- train_lm.sh
+++ train_lm.sh
@@ -1,20 +1,20 @@
 echo '<<< TRAINING AUTOREGRESSIVE TLM >>>'
 
 python train_LM.py --checkpoint '' \
-    --checkpoint_dir './checkpoints/t0_b1/' \
+    --checkpoint_dir './checkpoints/t0_b15/' \
     --model_config './experiment_configs/lm/decoder_test.yaml' \
-    --min_lr 1e-7 \
-    --max_lr 1e-5 \
-    --step_size 1700 \
+    --min_lr 1e-6 \
+    --max_lr 1e-4 \
+    --step_size 800 \
     --accumulate_gradients 1 \
     --clip_gradients \
     --clip_gradients_value 15 \
     --micro_batch_duration 0 \
-    --micro_batch_number 1 \
+    --micro_batch_number 15 \
     --max_allowed_utterance_gap 10.0 \
     --wandb_id '' \
     --save_top_k 1 \
-    --schedular_data './t0b1.json' \
+    --schedular_data './t0b15.json' \
     --project_name 'LM-context_len_stability_tests' \
     --label_smoothing 0.0 \
     
